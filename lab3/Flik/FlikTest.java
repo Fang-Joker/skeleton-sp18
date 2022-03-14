2383a2588eee72752af00522458ed4c81d853be9
Fang-Joker <593367432@qq.com>
2022-03-14 21:20:51 +0800

    0314

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void testIsSameNumber() {
+        assertTrue(Flik.isSameNumber(1, 1));
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,13 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3);
+        IntList exp = IntList.of(3, 2, 1);
+        assertEquals(exp, IntList.reverse(A));
+        assertEquals(A, IntList.reverse(A));
+        assertEquals(null, IntList.reverse(null));
+    }
+
 }
