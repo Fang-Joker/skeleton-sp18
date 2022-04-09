59db4677c48e32a00f6ed985f76b94e90f3ccb41
Fang-Joker <593367432@qq.com>
2022-04-09 12:33:39 +0800

    hw1 0409 v1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,11 +9,22 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> arb = new ArrayRingBuffer(10);
+        assertEquals(10, arb.capacity());
+        // arb.dequeue();
+        for (int i = 0; i < 10; i++) {
+            arb.enqueue(i * 1.1);
+        }
+        assertEquals(10, arb.fillCount());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arb.dequeue());
+            arb.enqueue(Math.random() - 0.5);
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+        // jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+        System.out.println((int)Math.round(12.12));
     }
 } 
